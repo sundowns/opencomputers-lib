2e049d962b876178240d1492f6ef1db6c3779f2f
sundowns <tom@smallridge.com.au>
2019-08-13 21:00:24 +0800

    cleaning up patrol

--- robots/patrol.lua
+++ robots/patrol.lua
@@ -1,64 +1,57 @@
 local robot = require("robot")
 local computer = require("computer")
 
-function choose_and_execute(...)
+local function choose_and_execute(...)
   local args = {...}
   args[math.random(1, #args)]()
 end
 
-function chance_to_act(weighting, callback)
-  local choice = math.random()
-  if choice < weighting then
-    callback()  
+local function randomly_turn()
+  choose_and_execute(robot.turnLeft, robot.turnRight)
+end
+
+local function chime()
+  computer.beep("--.-...--")
+end
+
+local function chance_to_act(weighting, callback)
+  if math.random() < weighting then
+    callback()
   end
 end
 
-function turn_until_unblocked(turn_callback)
-  local blocked, _ = robot.detect()  
+local function turn_until_unblocked(turn_callback)
+  local blocked, _ = robot.detect()
   local turns = 0
   while blocked and turns < 4 do
     turn_callback()
     turns = turns + 1
-    blocked, _ = robot.detect() 
+    blocked, _ = robot.detect()
   end
   if turns >= 4 then
-    robot.back()    
+    robot.back()
   end
 end
 
-function try_avoid_obstacle_infront()
-  local left = math.random(0,1) < 0.5
-  
+local function try_avoid_obstacle_infront()
   if math.random() < 0.5 then
     turn_until_unblocked(robot.turnLeft)
   else
-    turn_until_unblocked(robot.turnRight)     
+    turn_until_unblocked(robot.turnRight)
   end
 end
 
-function patrol()
+local function patrol()
   while true do
     local will_collide, obstacle = robot.detect()
-    if will_collide then
+    if will_collide or obstacle ~= "passable" then -- TODO:
       try_avoid_obstacle_infront()
     else
       chance_to_act(0.15, randomly_turn)
-      chance_to_act(0.3, wait)
       chance_to_act(0.025, chime)
       chance_to_act(0.65, robot.forward)
     end
   end
-end
-
-function randomly_turn()
-  choose_and_execute(robot.turnLeft, robot.turnRight)
-end
-
-function chime()
-  computer.beep("--.-...--")
-end
-
-function wait()
 end
 
 print("watch me run dad!")
--- trash/dumberer.lua
+++ trash/dumberer.lua
@@ -14,8 +14,8 @@ local words = {
   "misplaced"
 }
 
-while true do  
-  print("\n\n\n\n        you are " .. words[math.random(1,#words)])
+while true do
+  print("\n\n\n\n        you are " .. words[math.random(1, #words)])
   os.execute("sleep 2")
   os.execute("clear")
 end
